032f9390fd6b0aec857e04c60b6972dda4141a58
GIGLIONI-Maellys <159936244+GIGLIONI-Maellys@users.noreply.github.com>
2025-05-11 18:15:02 +0200

    Add files via upload

--- app/views/index.php
+++ app/views/index.php
@@ -4,7 +4,20 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Daktari</title>
+    <link rel="stylesheet" href="style/cnx.css">
 </head>
 <body>
+    <div class="login_Container">
+        <h1>
+            (Re)Bonjour !
+        </h1>
+        <form action="index.php" method="get">
+            <p>Utilisateur</p>
+            <input type="text" name="login" required>
+            <p>Mot de passe</p>
+            <input type="text" name="mdp" required>
+            <input id="submitBtn" type="submit" value="Se connecter">
+        </form>
+    </div>
 </body>
 </html>
