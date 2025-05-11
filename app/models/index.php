f8f0e2797b8b72594de67e11f6c3931c20bdae1c
GIGLIONI-Maellys <159936244+GIGLIONI-Maellys@users.noreply.github.com>
2025-05-11 18:14:34 +0200

    Add files via upload

--- app/models/index.php
+++ app/models/index.php
@@ -1,10 +1,10 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Daktari</title>
-</head>
-<body>
-</body>
-</html>
+<?php
+
+    //on essaye de se connecter à la base de données
+    include($oringinDir.'/config/connexion_db.php');
+
+    //On fait une requête de vérification pour savoir si le login et le mdp existe
+    //On récupère le hash
+    $res = $cnx->query("SELECT * FROM user_db where login = ".$cnx->quote($login)." and mpd = md5(".$cnx->quote($mdp).")");
+    //on renvoit le resultat obtenu au controleur
+?>
