58856f894fdd5b37f16223b4b0b11d1f428b837f
GIGLIONI-Maellys <159936244+GIGLIONI-Maellys@users.noreply.github.com>
2025-05-14 11:37:14 +0200

    Add files via upload

--- app/models/index.php
+++ app/models/index.php
@@ -1,10 +1,10 @@
 <?php
 
     //on essaye de se connecter à la base de données
-    include($oringinDir.'/config/connexion_db.php');
+    include($originDir.'/config/connexion_db.php');
 
     //On fait une requête de vérification pour savoir si le login et le mdp existe
     //On récupère le hash
-    $res = $cnx->query("SELECT * FROM user_db where login = ".$cnx->quote($login)." and mpd = md5(".$cnx->quote($mdp).")");
+    $res = $cnx->query("SELECT * FROM user_db WHERE username=".$cnx->quote($login)." and password_hash = md5(".$cnx->quote($mdp).");");
     //on renvoit le resultat obtenu au controleur
 ?>
