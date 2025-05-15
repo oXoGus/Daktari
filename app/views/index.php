3fcc292907fa4cf60db8b4ae02bb0f9b9602075c
GIGLIONI-Maellys <159936244+GIGLIONI-Maellys@users.noreply.github.com>
2025-05-15 23:16:23 +0200

    Add files via upload

---
+++ app/views/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Daktari</title>
+    <link rel="stylesheet" href="style/style.css">
+</head>
+<body>
+    <div class="login_Container">
+        <h1>
+            Bonjour, <?php echo $login ?>
+        </h1>
+        <div><a href="animal_ajouter.php">ajouter animal</a></div>
+    </div>
+</body>
+</html>
