023c907cd8a9975ae5358a39d4ec2486357d1eed
GIGLIONI-Maellys <159936244+GIGLIONI-Maellys@users.noreply.github.com>
2025-05-23 01:24:53 +0200

    Add files via upload

--- app/controllers/stats.php
+++ app/controllers/stats.php
@@ -1,4 +1,8 @@
 <?php
-    $originDir = __DIR__."/..";
-    include($originDir."/app/controllers/stats.php");
-    ?>
+    include($originDir."/config/middleware.php"); // page accessible uniquement aux utilisateur connecté
+    include ($originDir."/app/models/stats.php");
+    // si l'utilisateur est connecté 
+    // on inclue directment la vue puiqu'il n'y à aucune info à récup
+    include($originDir."/app/views/stats.php");
+    
+?>
