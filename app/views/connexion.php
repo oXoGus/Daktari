b9a1f70d8507b735d3fdd15059e79a43c55a384f
oXoGus <mdintrat0205@gmail.com>
2025-05-22 09:34:45 +0200

    login

--- app/views/connexion.php
+++ app/views/connexion.php
@@ -1,10 +1,41 @@
-<?php
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap" rel="stylesheet">
+    <title>Daktari</title>
+</head>
+<body>
 
-    //on essaye de se connecter à la base de données
-    include($originDir.'/config/connexion_db.php');
+    
 
-    //On fait une requête de vérification pour savoir si le login et le mdp existe
-    //On récupère le hash
-    $res = $cnx->query("SELECT * FROM user_db WHERE username=".$cnx->quote($login)." and password_hash = md5(".$cnx->quote($password).")");
-    //on renvoit le resultat obtenu au controleur
-?>
+    <div class="mainContainer">
+        <div class="loginContainer">
+            <h1>(Re)Bonjour !</h1>
+            <form action="connexion.php" method="get">
+                <p>Utilisateur</p>
+                <input type="text" autocomplete="off" name="login" required>
+                <p>Mot de passe</p>
+                <div class="inputContainer">
+                    <input id="mdp" type="password" name="mdp" required>
+                    <button type="button" id="btnEye" class="btnEye" onclick="changerVisibiliteMDP('mdp', 'btnEye')" ></button>
+                </div>
+                <input type="submit" value="Se connecter">
+            </form>
+            <?php 
+                if (isset($err)){
+                    echo "<div class=\"errLogin\">";
+                        echo "<p>$err</p>";
+                    echo "</div>";
+                    unset($err);
+                } 
+            ?>
+        </div>    
+    </div>
+    <script src="script/affichageMDP.js"></script>
+</body>
+</html>
